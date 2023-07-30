045035de6fa99459ae628f6debd2eb81978b953d
tiodwisatrio <tiodwisatrio27@gmail.com>
2023-07-31 01:02:13 +0700

    initial commit

---
+++ tests/class.test.ts
@@ -0,0 +1,15 @@
+describe('CLASS', () => { 
+    it("should can create class", () => {
+        class Customer {
+
+        }
+
+        class Order {
+
+        }
+
+
+        const customer: Customer = new Customer();
+        const order: Order = new Order();
+    })
+ })
