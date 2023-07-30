2808f969d753e1ba10ce6bf0773210df47ccd882
tiodwisatrio <tiodwisatrio27@gmail.com>
2023-07-31 01:11:07 +0700

    constructor

--- tests/class.test.ts
+++ tests/class.test.ts
@@ -1,15 +1,23 @@
 describe('CLASS', () => { 
+
+    class Customer {
+        constructor() {
+            console.log("Create new customer")
+        }
+    }
+
+    class Order {
+
+    }
+
+
     it("should can create class", () => {
-        class Customer {
-
-        }
-
-        class Order {
-
-        }
-
-
         const customer: Customer = new Customer();
         const order = new Order();
+    })
+
+    it("should can create constructor", () => {
+        new Customer();
+        new Customer();
     })
  })
