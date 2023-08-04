6a3467f3471989c6431197737ad2763ae0055bb2
tiodwisatrio <tiodwisatrio27@gmail.com>
2023-08-04 19:58:40 +0700

    method

--- tests/properties.test.ts
+++ tests/properties.test.ts
@@ -9,6 +9,10 @@ describe("Properties", () => {
             this.id = id;
             this.name = name;
         }
+
+        sayHello(name: string): void {
+            console.log(`Hello ${name}, my name is ${this.name}`)
+        }
     }
 
     it("Should can have properties", () => {
@@ -18,6 +22,16 @@ describe("Properties", () => {
 
         expect(customer.id).toBe(1);
         expect(customer.name).toBe("Jhon");
+    })
+
+    it("Should can have method", () => {
+        const customer = new Customer(2, "Budi");
+        customer.sayHello("Joko");
+
+        expect(customer.id).toBe(2);
+        expect(customer.name).toBe("Budi");
+        expect(customer.sayHello).toBeDefined();
+
     })
 
     
