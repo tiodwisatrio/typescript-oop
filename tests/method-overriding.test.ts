757f6f1bf186915bf7ffe08194d016791b83159a
tiodwisatrio <tiodwisatrio27@gmail.com>
2023-08-05 19:40:27 +0700

    Method Overriding

---
+++ tests/method-overriding.test.ts
@@ -0,0 +1,31 @@
+describe("Method Overriding", () => {
+    // Mendeklarasikan ulang method yang ada di parent class
+
+    class Employee {
+        name: string;
+
+        constructor(name: string) {
+            this.name = name
+        }
+
+        sayHello(name: string): void {
+            console.log(`Hello ${name}, my name is ${this.name}`)
+        }
+    }
+
+    class Manager extends Employee {
+
+        sayHello(name: string): void {
+            console.log(`Hello ${name}, my name is ${this.name} and I'm a manager`)
+        }
+    }
+
+
+    it("Should support method overriding", () => {
+        const employee = new Employee("Tio");
+        employee.sayHello("Budi");
+
+        const manager = new Manager("Tio");
+        manager.sayHello("Budi");
+    })
+})
