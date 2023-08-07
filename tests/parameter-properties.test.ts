960f5c23a7b8b8d72e477e08f0489f031e4eb21a
tiodwisatrio <tiodwisatrio27@gmail.com>
2023-08-07 21:31:58 +0700

    add customer properties

--- tests/parameter-properties.test.ts
+++ tests/parameter-properties.test.ts
@@ -6,6 +6,12 @@ describe('Parameter Properties', () => {
         }
     }
 
+    class Customer {
+        constructor(public idCustomer: string) {
+
+        }
+    }
+
     it("Support parameter properties", () => {
         const person = new Person("Tio");
         console.log(person.name)
@@ -14,5 +20,11 @@ describe('Parameter Properties', () => {
         person.name = "Joko";
         console.log(person.name)
         expect(person.name).toBe("Joko")
+    })
+
+    it("Support parameter properties", () => {
+        const customer = new Customer("C001");
+        console.log(customer.idCustomer)
+        expect(customer.idCustomer).toBe("C001");
     })
  })
